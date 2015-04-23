e7a17752a1ac06996e9ac5b38130f062a40706c9
annalandstrom <sweco@Swecos-MacBook-Pro.local>
2015-04-23 14:38:00 +0200

    ljhg

--- app/widgets/interactive/controllers/widget.js
+++ app/widgets/interactive/controllers/widget.js
@@ -1,8 +1,17 @@
-
-
+function showInteractive() {
+	if (!interactiveVisible) {
+		$.interactiveView.show();
+		interactiveVisible = true;
+	} else {
+		$.interactiveView.hide();
+		interactiveVisible = false;
+	}	
+}
 
 function closeInteractive() {
 	$.interactiveView.hide();
 	interactiveVisible = false;
 }
+
+Alloy.Globals.showInteractive = showInteractive;
 Alloy.Globals.closeInteractive = closeInteractive;
