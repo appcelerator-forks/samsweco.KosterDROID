3f1ed24487d7d8fd19a36bac4a3b07d6068500c9
madelenejohnsson <madelenjohnsson@hotmail.com>
2015-05-12 12:03:19 +0200

    äntligen! Rock on

--- app/controllers/infoDetail.js
+++ app/controllers/infoDetail.js
@@ -1,4 +1,5 @@
 var args = arguments[0] || {};
+var urlCollection = Alloy.Collections.info_urlModel;
 
 //-----------------------------------------------------------
 // Args skickade från listvy
@@ -6,20 +7,99 @@ var args = arguments[0] || {};
 try {
 	$.lblInfoTitle.text = args.name || "Title";
 	$.lblInfoText.text = args.infoTxt || "Info";
-	$.lblInfoLink.text = args.desc || "url";
 	$.infoImg.image = "/pics/" + args.img;
-	
-	var infoid = args.id;
-	var url = args.link;
-
-	var link = $.lblInfoLink;
-	link.addEventListener('click', function(e) {
-		openLink(url);
-	});
+	var id = args.id;
 	
 } catch(e) {
 	newError("Något gick fel när sidan skulle laddas, prova igen!", "infoDetail - load data into labels");
 }
+
+setRowData();
+
+function showinfoDetails(info) {
+	try {
+		var selectedInfo = info.row;
+		var args = {
+			id : selectedInfo.id,
+			name : selectedInfo.name,
+			infoTxt : selectedInfo.infoTxt,
+			link : selectedInfo.link,
+			img : selectedInfo.image,
+			desc : selectedInfo.desc
+		};
+
+		var infoDetail = Alloy.createController("infoDetail", args).getView();
+		infoDetail.open();
+	} catch(e) {
+		newError("Något gick fel när sidan skulle laddas, prova igen!", "infoDetail - showInfoDetail");
+	}
+}
+
+//-----------------------------------------------------------
+// sätter alla items i listan
+//-----------------------------------------------------------
+function setRowData() {
+	try {
+		var tableViewData = [];
+		urlCollection.fetch({
+			query : 'SELECT * from info_urlModel WHERE info_id ="' + id + '"'
+		});
+
+		var urlJson = urlCollection.toJSON();
+
+		for (var i = 0; i < urlJson.length; i++) {
+			var row = Ti.UI.createTableViewRow({
+				id : urlJson[i].id,
+				height : '60dp',
+				top : '0dp',
+				hasChild : true
+			});
+
+			var linkName = Ti.UI.createLabel({
+				width : Ti.UI.FILL,
+				left : '15dp',
+				right : '15dp',
+				font : {
+					fontSize : '13dp',
+				},
+				color : '#0098C3',
+				text : urlJson[i].linkname
+			});
+
+			row.add(linkName);
+			tableViewData.push(row);
+		}
+
+		if (tableViewData.length > 0) {
+			$.tableView.data = tableViewData;
+			$.lblLink.show();
+		}
+		else {
+			$.lblLink.hide();
+			$.tableView.height = 0;
+		}
+
+	} catch(e) {
+		newError("Något gick fel när sidan skulle laddas, prova igen!", "infoList - setRowData");
+	}
+}
+
+//-----------------------------------------------------------
+// Hämtar all info som ska läsas in i listan
+//-----------------------------------------------------------
+function getLink(e) {
+
+	var rowId = e.rowData.id;
+	urlCollection.fetch({
+		query : 'SELECT url FROM info_urlModel WHERE id = "' + rowId + '"'
+	});
+
+	var jsonObj = urlCollection.toJSON();
+	var web = jsonObj[0].url;
+
+	openLink(web);
+}
+
 
 //-----------------------------------------------------------
 // Öppnar url'en i en webView.
